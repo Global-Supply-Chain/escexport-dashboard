b582c680196d2caf9093d73078ec2fb19e4e0232
aunghp <aunghp.io@gmail.com>
2023-11-16 10:55:53 +0630

    fix token redirect

--- src/layouts/default/index.js
+++ src/layouts/default/index.js
@@ -4,6 +4,7 @@ import { AppSidebar } from "./components/AppSidebar";
 import { Outlet, useNavigate } from 'react-router-dom';
 import { getData } from '../../helpers/localstorage';
 import { keys } from '../../constants/config';
+import { useEffect } from 'react';
 
 const layoutOptions = {
     cssTransition: true,
@@ -15,23 +16,27 @@ export const DefaultLayout = () => {
     const token = getData(keys.API_TOKEN);
     const navigate = useNavigate();
 
+    useEffect(() => {
+        if(!token) {
+            navigate('/auth/login');
+        }
+    },[token, navigate]);
+
     return(
-        <>
-        
-        {token ? (
-             <PrimeReactProvider value={layoutOptions}>
-             <div className="wrapper">
-                 <AppToolbar />
-                 <div className='app-container'>
-                    <AppSidebar />
-                     <div className='app-content'>
-                         <Outlet />
-                     </div>
-                 </div>
-             </div>
-         </PrimeReactProvider>
-        ) : navigate('/auth/login')}
-        
+        <> 
+            { token && (
+                <PrimeReactProvider value={layoutOptions}>
+                    <div className="wrapper">
+                        <AppToolbar />
+                        <div className='app-container'>
+                        <AppSidebar />
+                            <div className='app-content'>
+                                <Outlet />
+                            </div>
+                        </div>
+                    </div>
+                </PrimeReactProvider>
+            )}
         </>
     )
 }
