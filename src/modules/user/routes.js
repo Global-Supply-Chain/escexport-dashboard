99f55b94eb7887757636238aa1d121ab5499c986
ShinKhantXoX <shinkhantlaybal@gmail.com>
2023-11-17 15:50:24 +0630

    add user create

--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -1,10 +1,15 @@
 import { paths } from "../../constants/paths";
 import { UserList } from "./view/UserList";
+import { UserCreate } from "./entry/UserCreate";
 
 
 export const userRoutes = [
     {
         path : paths.user,
         element : <UserList />
+    },
+    {
+        path: paths.userCreate,
+        element : <UserCreate />
     }
 ]
