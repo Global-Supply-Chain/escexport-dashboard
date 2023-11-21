008fd3c68df42d1e9582a7d45b761a92e38d2eec
aunghp <aunghp.io@gmail.com>
2023-11-21 14:00:48 +0630

    update admin feature

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,6 +1,10 @@
+export const baseURL = "http://localhost:8000/dashboard";
+
 export const endpoints = {
     login: "auth/login",
     admin: "admin",
     user: "user",
-    promotion: "promotion"
+    promotion: "promotion",
+    image: `${baseURL}/media`,
+    status: "status",
 }
