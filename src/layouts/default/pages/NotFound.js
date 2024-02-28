23e0a3db9aaa9a690aa5ef7f18956cc7e0f59e0e
ShinKhantXoX <shinkhantlaybal@gmail.com>
2024-02-28 11:48:32 +0630

    add not found

--- src/layouts/default/pages/NotFound.js
+++ src/layouts/default/pages/NotFound.js
@@ -1,5 +1,38 @@
+import { Image } from "primereact/image";
+import { useNavigate } from "react-router-dom"
+import Logo from "../../../assets/images/logo.jpeg"
+
 export const NotFound = () => {
-    return(
-        <h4> Page Not Found </h4>
+
+    const navigate = useNavigate();
+
+    return (
+        <div
+            style={{
+                height: '100vh',
+                overflow : 'hidden'
+            }}
+            className="flex align-items-center justify-content-center relative">
+
+            <div>
+
+                <div className=" not-found-circle-one"></div>
+                <div className=" not-found-circle-two"></div>
+
+                <div className="flex align-items-center justify-content-center">
+                    <Image 
+                        src={Logo} 
+                        className="not-found-logo"
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    />
+                </div>
+
+                <h1>Oops, Not Found Page</h1>
+
+            </div>
+
+        </div>
     )
 }
