e8fc745be302fb4297b20a370dc60523d273a19b
ShinKhantXoX <shinkhantlaybal@gmail.com>
2024-06-05 16:51:47 +0630

    add promotion item

--- src/modules/promotion/entry/CreatePromotionItem.js
+++ src/modules/promotion/entry/CreatePromotionItem.js
@@ -3,29 +3,63 @@ import { tooltipOptions } from "../../../constants/config";
 import { useCallback, useEffect, useState } from "react";
 import { payloadHandler } from "../../../helpers/handler";
 import { ValidationMessage } from "../../../shares/ValidationMessage";
-import { getRequest } from "../../../helpers/api";
+import { getRequest, postRequest } from "../../../helpers/api";
 import { endpoints } from "../../../constants/endpoints";
 import { useParams } from "react-router-dom";
+import { MultiSelect } from 'primereact/multiselect';
+import { Button } from "primereact/button";
+import { updateNotification } from "../../../shares/shareSlice";
+import { useDispatch } from "react-redux";
 
-export const CreatePromotionItem = () => {
+export const CreatePromotionItem = ({id, setStatus}) => {
 
     const [loading, setLoading] = useState(false);
     const [payload, setPayload] = useState({
         promotion_price: "",
         item_ids: []
     });
+    const [itemLists, setItemLists] = useState([]);
+    const [selectedItem, setSelectedItem] = useState([]);
 
     const params = useParams();
+    const dispatch = useDispatch();
 
     const initLoading = useCallback(async () => {
-        const result = await getRequest(`${endpoints.item}`);
-
-        console.log(result);
+        const result = await getRequest(`${endpoints.item}?status=ACTIVE`);
+        if(result.status === 200){
+            const formatData = result.data.map((item) => {
+                return {
+                    name : item.name,
+                    code: item.id
+                }
+            })
+            setItemLists(formatData)
+        }
     },[]);
 
     useEffect(() => {
         initLoading();
     }, [initLoading]);
+
+    const submit = async () => {
+        try {
+
+            const response = await postRequest(`${endpoints.promotion}/${id}/item`, payload);
+            if(response.status === 200){
+                dispatch(updateNotification({
+                    show: true,
+                    summary: "Success",
+                    severity: "success",
+                    detail: 'Promotion in item is created successfully'
+                  }));
+                setStatus(response.status)
+            }
+
+        } catch (error) {
+            console.error('Promotion in item store', error);
+        }
+    }
+    
 
     return (
         <div className="grid">
@@ -51,8 +85,36 @@ export const CreatePromotionItem = () => {
 
             <div className="col-12 md:col-8 lg:col-8 py-3">
                 <div className="flex flex-column gap-2">
-                    <label htmlFor="title" className='text-black'> Choose Items </label>
+                    <label htmlFor="item" className='text-black'> Choose Items </label>
+                    <MultiSelect 
+                        value={selectedItem} 
+                        onChange={(e) => {
+                            setSelectedItem(e.value)
+                            const itemLists = e.value.map((item) => {
+                                return item.code
+                            })
+                            setPayload({
+                                ...payload,
+                                item_ids: itemLists
+                            })
+                        }} 
+                        options={itemLists} 
+                        optionLabel="name" 
+                        placeholder="Select promotion item"
+                        className="w-full md:w-20rem" 
+                    />
+                  <ValidationMessage field={"item_id"} />
                 </div>
+            </div>
+
+            <div className="col-12 flex align-items-center justify-content-end">
+                    <Button 
+                        size="small"
+                        severity="danger"
+                        onClick={submit}
+                    >
+                        Submit
+                    </Button>
             </div>
         </div>
 
--- src/modules/promotion/list/PromotionItemView.js
+++ src/modules/promotion/list/PromotionItemView.js
@@ -1,13 +1,20 @@
 import { Card } from "primereact/card"
 import { CreatePromotionItem } from "../entry/CreatePromotionItem"
+import { PromotionItemList } from "../view/PromotionItemList";
+import { useState } from "react";
 
-export const PromotionItemView = () => {
+export const PromotionItemView = ({id}) => {
+
+    const [status, setStatus] = useState();
 
     return(
         <Card title="Promotion Item List">
             <div className="grid">
                 <div className="col-12">
-                    <CreatePromotionItem />
+                    <CreatePromotionItem id={id} setStatus={setStatus} />
+                </div>
+                <div className=" col-12">
+                    <PromotionItemList id={id} status={status} />
                 </div>
             </div>
         </Card>
